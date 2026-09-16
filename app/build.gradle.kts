07541d9246e9e4a4d180d3bc56bbdd54d73758b6
yoshiboshi3 <150073352+yoshiboshi3@users.noreply.github.com>
2026-09-15 20:26:17 -0500

    Add files via upload

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.xrealcanvas.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.xrealcanvas.app"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
